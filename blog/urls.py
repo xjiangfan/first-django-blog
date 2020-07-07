1a754b969b62a4492c9cf510c9e518ec68c931ab
jiangfan <fanjiang1116@gmail.com>
2020-07-07 17:07:49 +0800

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
 	
     re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+	
+	re_path(r'^post/new/$', views.post_new, name='post_new'),
+	
+	re_path(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
